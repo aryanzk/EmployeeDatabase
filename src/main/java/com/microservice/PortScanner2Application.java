5474a1edd5d8452c12b43f9d90e64beed8b93f84
aryanzk <108435423+aryanzk@users.noreply.github.com>
2022-07-09 06:16:39 +0500

    mine

---
+++ src/main/java/com/microservice/PortScanner2Application.java
@@ -0,0 +1,13 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortScanner2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortScanner2Application.class, args);
+    }
+
+}
